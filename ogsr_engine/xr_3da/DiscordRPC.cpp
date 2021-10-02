aa2c707640384d5883889276a9470860204c1b8b
i-love-kfc <stepadunaevskij@gmail.com>
2021-10-02 17:34:28 +0400

    Добавил исходники DiscordRPC

--- ogsr_engine/xr_3da/DiscordRPC.cpp
+++ ogsr_engine/xr_3da/DiscordRPC.cpp
@@ -2,32 +2,10 @@
 
 #include "DiscordRPC.hpp"
 
-constexpr char* DISCORD_LIBRARY_DLL{ "discord-rpc.dll" };
-
 ENGINE_API DiscordRPC Discord;
 
 void DiscordRPC::Init()
 {
-	m_hDiscordDLL = LoadLibrary(DISCORD_LIBRARY_DLL);
-	if (!m_hDiscordDLL)
-	{
-		Msg("!![%s]Failed to load [%s], error: %s", __FUNCTION__, DISCORD_LIBRARY_DLL, Debug.error2string(GetLastError()));
-		return;
-	}
-
-	Discord_Initialize = (pDiscord_Initialize)GetProcAddress(m_hDiscordDLL, "Discord_Initialize");
-	Discord_Shutdown = (pDiscord_Shutdown)GetProcAddress(m_hDiscordDLL, "Discord_Shutdown");
-	Discord_ClearPresence = (pDiscord_ClearPresence)GetProcAddress(m_hDiscordDLL, "Discord_ClearPresence");
-	Discord_RunCallbacks = (pDiscord_RunCallbacks)GetProcAddress(m_hDiscordDLL, "Discord_RunCallbacks");
-	Discord_UpdatePresence = (pDiscord_UpdatePresence)GetProcAddress(m_hDiscordDLL, "Discord_UpdatePresence");
-
-	if (!Discord_Initialize || !Discord_Shutdown || !Discord_ClearPresence || !Discord_RunCallbacks || !Discord_UpdatePresence) {
-		Msg("!![%s] Initialization failed!", __FUNCTION__);
-		FreeLibrary(m_hDiscordDLL);
-		m_hDiscordDLL = nullptr;
-		return;
-	}
-
 	DiscordEventHandlers nullHandlers{};
 	Discord_Initialize("862971629810221086", &nullHandlers, TRUE, nullptr);
 
@@ -36,19 +14,13 @@ void DiscordRPC::Init()
 
 DiscordRPC::~DiscordRPC()
 {
-	if (!m_hDiscordDLL) return;
-
 	Discord_ClearPresence();
 	Discord_Shutdown();
-
-	FreeLibrary(m_hDiscordDLL);
 }
 
 
 void DiscordRPC::Update(const char* level_name)
 {
-	if (!m_hDiscordDLL) return;
-
 	DiscordRichPresence presenseInfo{};
 
 	presenseInfo.startTimestamp = start_time; //время с момента запуска
