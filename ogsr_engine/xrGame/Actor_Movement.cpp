8beaf09ddb20e47867125eaa509c455e4bc959af
i-love-kfc <stepadunaevskij@gmail.com>
2022-03-21 17:35:03 +0400

    Update Actor_Movement.cpp

--- ogsr_engine/xrGame/Actor_Movement.cpp
+++ ogsr_engine/xrGame/Actor_Movement.cpp
@@ -360,16 +360,13 @@ void CActor::g_cl_CheckControls(u32 mstate_wf, Fvector &vControlAccel, float &Ju
 	{
 		LPCSTR state_anm = nullptr;
 
-		if (mstate_real & mcJump && !(mstate_old & mcJump)) {
-			PIItem itm = inventory().ItemFromSlot(inventory().GetActiveSlot());
+		if (mstate_real & mcJump && !(mstate_old & mcJump))
 			state_anm = "jump";
-			if(auto wpn = smart_cast<CWeapon*>(itm)) {
-				if(wpn->IsZoomed())
-					state_anm = "jump_aim";
-				state_anm = READ_IF_EXISTS(pSettings, r_string, wpn->HudSection().c_str(), std::string("cam" + std::string(state_anm)).c_str(), state_anm);
-			}
-		}
-		if (mstate_real & mcSprint && !(mstate_old & mcSprint))
+		else if (mstate_real & mcCrouch && !(mstate_old & mcCrouch)) 
+			state_anm = "crouch_down";
+		else if (mstate_real & mcCrouch && !(mstate_wf & mcCrouch))
+			state_anm = "crouch_up";
+		else if (mstate_real & mcSprint && !(mstate_old & mcSprint))
 			state_anm = "sprint";
 		else if (mstate_real & mcLStrafe && !(mstate_old & mcLStrafe))
 			state_anm = "strafe_left";
@@ -382,6 +379,14 @@ void CActor::g_cl_CheckControls(u32 mstate_wf, Fvector &vControlAccel, float &Ju
 
 		if (state_anm)
 		{ // play moving cam effect
+			if(inventory().GetActiveSlot()) {
+				if(PIItem itm = inventory().ItemFromSlot(inventory().GetActiveSlot()); const auto wpn = smart_cast<CWeapon*>(itm)) {
+					if(wpn->IsZoomed())
+						state_anm = std::string(std::string(state_anm) + "_aim").c_str();
+					state_anm = READ_IF_EXISTS(pSettings, r_string, wpn->HudSection().c_str(), std::string("cam_" + std::string(state_anm)).c_str(), state_anm);
+				}
+			}
+
 			auto control_entity = smart_cast<CActor*>(Level().CurrentControlEntity());
 			R_ASSERT2(control_entity, "current control entity is NULL");
 			CEffectorCam* ec = control_entity->Cameras().GetCamEffector(eCEActorMoving);
